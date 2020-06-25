78a0a98cb8f2c464df14639f9748d4174c3e1bfd
c-champagne <cassielchampagne@gmail.com>
2020-06-25 00:11:37 -0400

    Some visual setup

---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Test extends Component {
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
+export default Test;
---
+++ src/components/Window.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import './Window.css';
+import Item from './Item.js';
+import Test from './Test';
+
+export class Window extends Component {
+    render() {
+        return (
+            <div className="mainWindow">
+                <div className="background">
+                    <img src="/images/bedroom.jpg"/>
+                    <div className="dialogueBox">
+                        <div className="speaker">
+                            <img src="/images/characterModels/actresslaugh.PNG"/>
+                        </div>
+                        <div className="textBox">
+                            This is where the story text will go. Things will be happening here.  These are words in the story.<br/>
+                            <ul>
+                                <li>Player Choice</li>
+                                <li>Player Choice</li>
+                                <li>Player Choice</li>
+                            </ul>
+                        </div>
+                        <div className="inventory">
+                            <h2>Your Inventory</h2>
+                            <div>{Item}</div>
+                            <ul>
+                                <li></li>
+                                <li>Clue 2</li>
+                                <li>Clue 2</li>
+                                <li>Clue 2</li>
+                                <li>Clue 2</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Window
